8bbb64fff9cc5e2a339ff5434eebe57a2be7a645
James <xtreme.maicolxt94@gmail.com>
2019-03-30 22:14:21 +0000

    Fix dhs loaders

--- dhs/loaders/kwa/register.js
+++ dhs/loaders/kwa/register.js
@@ -1,7 +1,7 @@
 // Kodhe - Copyright 2019©
 // KWA template loader
 
-import  def from '../../../../std/package/kwa/register'
+import  def from '../../../std/package/kwa/register'
 
 export default def
-export * from '../../../../std/package/kwa/register'
+export * from '../../../std/package/kwa/register'
