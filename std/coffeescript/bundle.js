fba133d42d88a278226985a04644ec6b802f268d
James <xtreme.maicolxt94@gmail.com>
2019-04-04 20:56:08 +0000

    Update coffeescript to 2.4.0

--- std/coffeescript/bundle.js
+++ std/coffeescript/bundle.js
@@ -4,15 +4,15 @@ import Path from 'path'
 
 var init= async function(){
     var reg= new Registry()
-    var moduleinfo= await reg.resolve("coffeescript","^2.3.2")
-    var path= moduleinfo.folder 
+    var moduleinfo= await reg.resolve("coffeescript","^2.4.0")
+    var path= moduleinfo.folder
 
-    
-    
+
+
     var outfile= Path.join(__dirname, "runtime.js")
 
     var bundler= new Bundler(path)
-    bundler.packageJsonSupport= true 
+    bundler.packageJsonSupport= true
     bundler.disableTranspile=true
     bundler.virtualName= `coffeescript$v$${moduleinfo.version}`
     await bundler.writeToFile(outfile).bundle()
