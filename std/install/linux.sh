12a638e675a5ea58fa0abceefd3e8a8112b2e191
James <xtreme.maicolxt94@gmail.com>
2019-04-29 23:47:15 +0000

    update linux install to curl

--- std/install/linux.sh
+++ std/install/linux.sh
@@ -15,12 +15,12 @@ cp kwcore.install.js "$_HOME/kwcore.install.js"
 cd $_HOME
 
 # download kwcore.install.js
-wget "https://kwx.kodhe.com/x/std/install/kwcore.install.js" -O "$_HOME/kwcore.install.js"
+curl -L "https://kwx.kodhe.com/x/std/install/kwcore.install.js" -O "$_HOME/kwcore.install.js"
 
 # download nodejs 
-wget "https://raw.githubusercontent.com/voxsoftware/node-binaries/master/linux/x64/10.15.3/node.gz" -O "$_HOME/node.gz"
-wget "https://kwx.kodhe.com/x/core/dist/kwcore.app.js" -O "$_HOME/kwcore.app.js"
-#wget "https://kwx.kodhe.com/x/std/install/dist/kwcore.app.js" -O "$_HOME/kwcore.app.js"
+curl -L "https://raw.githubusercontent.com/voxsoftware/node-binaries/master/linux/x64/10.15.3/node.gz" -o "$_HOME/node.gz"
+curl -L "https://kwx.kodhe.com/x/core/dist/kwcore.app.js" -o "$_HOME/kwcore.app.js"
+
 
 
 rm node 2> /dev/null
@@ -32,7 +32,7 @@ chmod +x node
 
 # now symLink 
 rm -r "$_HOME/kwcore"
-wget "https://kwx.kodhe.com/x/std/install/kwcore" -O "$_HOME/kwcore"
+curl -L "https://kwx.kodhe.com/x/std/install/kwcore" -o "$_HOME/kwcore"
 chmod +x "$_HOME/kwcore"
 
 if [ "$EUID" -eq 0 ]
@@ -43,15 +43,15 @@ fi
 
 # download icons 
 mkdir icons
-wget "https://raw.githubusercontent.com/voxsoftware/kawix/master/icons/16x16.png" -O icons/16x16.png
-wget "https://raw.githubusercontent.com/voxsoftware/kawix/master/icons/24x24.png" -O icons/24x24.png
-wget "https://raw.githubusercontent.com/voxsoftware/kawix/master/icons/32x32.png" -O icons/32x32.png
-wget "https://raw.githubusercontent.com/voxsoftware/kawix/master/icons/48x48.png" -O icons/48x48.png
-wget "https://raw.githubusercontent.com/voxsoftware/kawix/master/icons/64x64.png" -O icons/64x64.png
-wget "https://raw.githubusercontent.com/voxsoftware/kawix/master/icons/96x96.png" -O icons/96x96.png
-wget "https://raw.githubusercontent.com/voxsoftware/kawix/master/icons/128x128.png" -O icons/128x128.png
-wget "https://raw.githubusercontent.com/voxsoftware/kawix/master/icons/256x256.png" -O icons/256x256.png
-wget "https://raw.githubusercontent.com/voxsoftware/kawix/master/icons/512x512.png" -O icons/512x512.png
+curl "https://raw.githubusercontent.com/voxsoftware/kawix/master/icons/16x16.png" -o icons/16x16.png
+curl "https://raw.githubusercontent.com/voxsoftware/kawix/master/icons/24x24.png" -o icons/24x24.png
+curl "https://raw.githubusercontent.com/voxsoftware/kawix/master/icons/32x32.png" -o icons/32x32.png
+curl "https://raw.githubusercontent.com/voxsoftware/kawix/master/icons/48x48.png" -o icons/48x48.png
+curl "https://raw.githubusercontent.com/voxsoftware/kawix/master/icons/64x64.png" -o icons/64x64.png
+curl "https://raw.githubusercontent.com/voxsoftware/kawix/master/icons/96x96.png" -o icons/96x96.png
+curl "https://raw.githubusercontent.com/voxsoftware/kawix/master/icons/128x128.png" -o icons/128x128.png
+curl "https://raw.githubusercontent.com/voxsoftware/kawix/master/icons/256x256.png" -o icons/256x256.png
+curl "https://raw.githubusercontent.com/voxsoftware/kawix/master/icons/512x512.png" -o icons/512x512.png
 
 
 ./kwcore kwcore.install.js
