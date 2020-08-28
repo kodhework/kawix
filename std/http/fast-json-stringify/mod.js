4219cff3707b2c519c093ea879623d3b8d15833f
James Suárez <xtreme.maicolxt94@gmail.com>
2020-08-28 08:57:15 +0200

    KWA Fix

--- std/http/fast-json-stringify/mod.js
+++ std/http/fast-json-stringify/mod.js
@@ -2,7 +2,7 @@ import fs from 'fs'
 import Path from 'path'
 
 export async function kawixPreload(){
-	let m = "fast-json.stringify"
+	let m = "fast-json-stringify"
 	let v = "2.2.3"
 	let name= Path.join(__dirname, "..", "..", "..", "npm-deps", "dist", m, v +".js")
 	if(fs.existsSync(name)) module.exports= await import(name)
