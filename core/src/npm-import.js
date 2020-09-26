a7300dd7cf156dbe74ea9108a4f9da82b86439d1
James Suárez <xtreme.maicolxt94@gmail.com>
2020-09-27 00:05:03 +0200

    update

--- core/src/npm-import.js
+++ core/src/npm-import.js
@@ -3,18 +3,18 @@ var Registry
 //import Registry from '../../std/package/registry.js'
 
 var load= async function(){
+    var pack1 = require("../package.json")
     try{
-        var pack1 = require("../package.json")
         Registry= await KModule.import('/virtual/@kawix/std/package/registry.yarn')
         Registry= Registry.default || Registry
     }catch(e){
 
         /* try determine the latest version */
-        var pack = await KModule.import("gh+/kodhework/kawix/core/package.json")
+        //var pack = await KModule.import("gh+/kodhework/kawix/std/package.json")
         //if(pack.version < pack1.stdVersion) pack.version = pack1.stdVersion
 
 		// LOADLIB FROM GITHUB
-        await KModule.import('gh+/kodhework/kawix@std'+pack.version+'/std/dist/register.js')
+        await KModule.import('gh+/kodhework/kawix@std'+pack1.stdVersion+'/std/dist/register.js')
 
 		Registry= await KModule.import('/virtual/@kawix/std/package/registry.yarn')
         Registry= Registry.default || Registry
